36ab279cc2f480390dde13ff2552151f08fbb02c
Anuj054 <anujc054@gmail.com>
2024-11-01 12:55:38 +0530

    app.js coreected

--- src/app.js
+++ src/app.js
@@ -1,67 +1,20 @@
-// src/server.js
+// src/app.js
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 require("./config/dotenv");
-const app = require("./app");
-const http = require("http");
-const { Server } = require("socket.io");
-const { setSocketIo } = require("./app");
-const User = require("./models/User");
-const chatService = require("./services/chatService");
+const router = require("./controllers/mycontroller");
+const { setSocketIo } = require("./controllers/mycontroller");
 
-const PORT = process.env.PORT || 5000;
+const app = express();
 
-// Create HTTP server
-const server = http.createServer(app);
+// Connect to MongoDB
+connectDB();
 
-// Create a new Socket.IO server instance
-const io = new Server(server);
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
 
-// Set the io instance in your controller
-setSocketIo(io);
+app.use("/api", router);
 
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Hello World from Mayank");
-});
-
-// Listen for connection events
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  // Listen for messages being sent
-  socket.on("sendMessage", async ({ groupId, senderId, message }) => {
-    try {
-      // Find the user by senderId to check their role
-      const user = await User.findById(senderId);
-      if (!user) {
-        socket.emit("error", { message: "User not found" });
-        return;
-      }
-
-      // Check if the user is an admin
-      if (user.role !== "admin") {
-        socket.emit("error", { message: "Only admins can send messages" });
-        return;
-      }
-
-      // If the user is an admin, add the message to the chat
-      const chat = await chatService.addMessageToChat(
-        groupId,
-        senderId,
-        message
-      );
-      io.to(groupId).emit("messageReceived", chat); // Notify others in the group
-    } catch (error) {
-      socket.emit("error", { message: error.message });
-    }
-  });
-
-  // Optionally listen for other events
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
+module.exports.setSocketIo = setSocketIo;
